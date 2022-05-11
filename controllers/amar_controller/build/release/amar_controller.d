3bfae5f14661822460b6ebe74822bd7b12b5260c
Tahsinur Rahman <64746243+tahsinurRahman72@users.noreply.github.com>
2022-05-11 18:41:18 +0600

    Add files via upload

---
+++ controllers/amar_controller/build/release/amar_controller.d
@@ -0,0 +1,8 @@
+build/release/amar_controller.o: amar_controller.cpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/User/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\User\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Users/User/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h
